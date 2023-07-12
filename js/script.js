135d749ce91772cb04977b0ea7ab3aa4bd541e6a
Manvel1996 <mdanielyan1996@gmail.com>
2023-07-12 14:50:42 +0400

    finish task

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 function MasonryPrototype() {}
 
-MasonryPrototype.prototype.render = function (containerClass, settings) {
-  const container = document.querySelector(containerClass);
+MasonryPrototype.prototype.render = function (containerClassName, settings) {
+  const container = document.querySelector(containerClassName);
 
   if (!container) {
     return;
@@ -9,34 +9,61 @@ MasonryPrototype.prototype.render = function (containerClass, settings) {
 
   const columnWidth = settings.columnWidth || 200;
   const autoResize = settings.autoResize || false;
-  const gap = 5
+  const gap = settings.gap || 5;
 
   function positionItems() {
     const containerWidth = container.offsetWidth;
-    const columns = Math.floor(containerWidth / (columnWidth + gap));
+    container.style.position = "relative";
 
-    const paddingInline = autoResize
-      ? 0
-      : (containerWidth - columns * (columnWidth + gap)) / 2;
+    let columns = Math.floor(containerWidth / (columnWidth + gap));
+    if (columns === 0) {
+      columns = 1;
+    }
 
-    container.style.cssText = `
-      columns-count: ${columns};
-      padding-inline: ${paddingInline}px;
-      column-gap: ${gap}px;
-      column-width: ${columnWidth}px;
-    `;
+    const heightColumns = new Array(columns).fill(0);
+
+    const childrenData = Array.from(container.children).map((childNode) => {
+      return {
+        childNode,
+        currentWidth: columnWidth,
+        currentHeight: Math.floor(
+          (columnWidth * childNode.firstElementChild.naturalHeight) /
+            childNode.firstElementChild.naturalWidth
+        ),
+      };
+    });
+
+    childrenData.forEach((child) => {
+      const min = Math.min(...heightColumns);
+      const minIndex = heightColumns.indexOf(min);
+
+      child.childNode.style.position = "absolute";
+      child.childNode.style.top = `${heightColumns[minIndex]}px`;
+      child.childNode.style.left = `${
+        minIndex * columnWidth + gap * minIndex
+      }px`;
+      child.childNode.style.width = `${child.currentWidth}px`;
+      child.childNode.style.height = `${child.currentHeight}px`;
+
+      heightColumns[minIndex] += child.currentHeight + gap;
+    });
   }
 
   positionItems();
 
-  window.addEventListener("resize", positionItems);
+  if (autoResize) {
+    window.addEventListener("resize", () => {
+      positionItems();
+    });
+  }
 };
 
 const Masonry = new MasonryPrototype();
 
 window.addEventListener("DOMContentLoaded", () => {
   Masonry.render(".masonry", {
-    columnWidth: 200,
-    // autoResize: true,
+    columnWidth: 600,
+    autoResize: true,
+    gap: 5,
   });
 });
